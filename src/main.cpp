8cfe64d04da9e2d9a6bfbf32166a2b2fb907e53b
Amaan Khan <cs1240335@cse.iitd.ac.in>
2026-02-16 21:47:57 +0530

    removed comments mostly

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,7 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    
-    // Create and show the main window
     MainWindow window;
     window.show();
-
     return app.exec();
 }
